450b44c3c4033f9b47bb13307179a2ca3dbf4f5f
Robinson_Xavier <robinson.webzen@gmail.com>
2022-06-27 13:41:06 -0300

    Adcionado estilos novos e função de receber e renderizar mensagens

--- scripts/script.js
+++ scripts/script.js
@@ -1,4 +1,5 @@
 let lindoNome = prompt("Seu lindo nome:");
+let listaMensagens= [];
 
 //entrar no servidor
 
@@ -9,6 +10,10 @@ let entrando = () => {
     };
 
     let promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/participants", entrada);
+    console.log(promise);
+
+    receberMensagens();
+    
 }
 
 let online = () => {
@@ -17,19 +22,70 @@ let online = () => {
         name: lindoNome
     }
 
-    let promise = axios.post ("https://mock-api.driven.com.br/api/v6/uol/status", status);
+    let promise = axios.post("https://mock-api.driven.com.br/api/v6/uol/status", status);
+    console.log(promise);
 }
-
-entrando();
-setInterval(online, 5000);
 
 //acessar mensagens do servidor
 
+let receberMensagens = () => {
+
+    const promise = axios.get('https://mock-api.driven.com.br/api/v6/uol/messages');
+    promise.then(respostaRecebida)
+
+}
+
+let respostaRecebida = (resposta) => {
+
+    listaMensagens = resposta.data;
+
+    renderizarMensagem();
+}
+
+let spanMensagemStatus = (msg) => {
+    
+    return `<span class="status">(${msg.time}) <strong>${msg.from}<strong> entra na sala...</span>`
+}
+
+let spanMensagemTodos = (msg) => {
+    
+    return `<span class="todos">(${msg.time}) <strong>${msg.from}<strong> para <strong>Todos<strong>: ${msg.text}</span>`
+}
+
+let spanMensagemPrivada = (msg) => {
+    
+    return `<span class="privado">(${msg.time}) <strong>${msg.from}<strong> reservadamente para <strong>${msg.to}<strong>: ${msg.text}</span>`
+}
+
+let renderizarMensagem = (mensagens) => {
+
+    let conteudo = document.querySelector(".conteudo");
+    conteudo.innerHTML = "";
+
+    for(let i = 0; i < listaMensagens.length; i++) {
+        let mensagem = listaMensagens[i];
+
+        if(mensagem.type == "status") {
+            conteudo.innerHTML += spanMensagemStatus(mensagem);
+
+        } else if(mensagem.to !== "todos" || mensagem.to !== "status") {
+            conteudo.innerHTML += spanMensagemPrivada(mensagem);
+        }
+
+        conteudo.innerHTML += spanMensagemTodos(mensagem)
+    }
+}
+
 
 //enviar mensagens para o servidor
+
 
 //situação de acerto e de erro baseado na resposta do servidor
 
 //interações js, html e css
 
 //bonus
+
+
+entrando();
+setInterval(online, 5000);
